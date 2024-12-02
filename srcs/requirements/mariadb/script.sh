ee5479819c900ec86f30bc71c26dd115eb445651
abdelbassat_00 <abdoqoubai@gmail.com>
2024-12-02 11:53:35 +0100

    ..

---
+++ srcs/requirements/mariadb/script.sh
@@ -0,0 +1,17 @@
+
+
+
+# sudo apt-get purge mysql-common mariadb-common
+
+sudo apt update
+
+sudo apt upgrade -y
+
+
+sudo apt install mariadb-server 
+
+
+# sudo echo "root\nroot\n" | sudo  mysql_secure_installation 
+
+
+
