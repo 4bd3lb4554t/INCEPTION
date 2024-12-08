38b302d8b5f8c65f274fa42ba59e49501dea26b1
Abde lbassat Quaoubai <abquaoub@c4r10p3.1337.ma>
2024-12-08 21:32:59 +0100

    ..

--- srcs/requirements/nginx/script.sh
+++ srcs/requirements/nginx/script.sh
@@ -1,5 +1,12 @@
 
 #!/bin/bash
 
+# mv /nginx/wordpress /etc/nginx/sites-available/
+
+# ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
+
+# nginx -t
+
+# nginx -s reload
 
 nginx -g "daemon off;"
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-# mkdir  -p /var/www/wordpress
+apt install php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
 
-# mv /wordpress/index.html  /var/www/wordpress
+mkdir  -p /var/www/wordpress
 
+cd /var/www/
+
+apt install wget -y
+
+wget https://wordpress.org/latest.tar.gz
+
+tar -xvf latest.tar.gz
+
+# mv wordpress /var/www/wordpress
+
+chown -R www-data:www-data /var/www/wordpress
+
+chmod -R 755 /var/www/wordpress
+
+
+# while true  for   is container is running for test ??
+while true; do
+    echo "This is an infinite loop"
+    sleep 1  
+done
