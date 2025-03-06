99a9ba0537cda65e35f5205e588f62f897aa46d1
abdelbassat <abdoqoubai@gmail.com>
2025-03-06 21:05:58 +0000

    now using https

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-rm -rf /etc/nginx/sites-enabled/default
+
+apt update &&  apt install openssl
+
+openssl genpkey -algorithm RSA -out /etc/nginx/conf.d/ssl_certificate_key.pem -pkeyopt rsa_keygen_bits:2048 # generate private key
+
+
+openssl req -new -x509 -key /etc/nginx/conf.d/ssl_certificate_key.pem -out /etc/nginx/conf.d/ssl_certificate.pem -days 365 << end
+MA
+abdelbassat quaoubai
+UM6P
+1337
+inception
+abdelbassat
+abdoqoubai@gmail.com
+end
+
+
 
 nginx -g "daemon off;"
 
