8179deab065b498dc535a564f2bad81ca9dacdae
abdelbassat <abdoqoubai@gmail.com>
2025-03-09 06:01:10 +0000

    I happy today  9/03

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+
 apt update &&  apt install openssl
 
-
 openssl genpkey -algorithm RSA -out /etc/nginx/conf.d/ssl_certificate_key.pem -pkeyopt rsa_keygen_bits:2048
-
 
 openssl req -new -x509 -key /etc/nginx/conf.d/ssl_certificate_key.pem -out /etc/nginx/conf.d/ssl_certificate.pem -days 365 << end
 MA
@@ -16,6 +15,6 @@ abdelbassat
 abdoqoubai@gmail.com
 end
 
-chown -R nginx: /var/www/html/wordpress/
+chown -R nginx: /var/www/html/
 
 nginx -g "daemon off;"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,16 +4,18 @@ apt install php-fpm -y
 
 apt install unzip wget -y
 
-cd /var/www/html
+# cd /var/www/html
 
-wget https://wordpress.org/latest.zip
+# wget https://wordpress.org/latest.zip
 
-unzip latest.zip
+# unzip latest.zip
 
-# mv wordpress my-wordpress
+apt install sed -y 
 
-# cd my-wordpress
+sed -i 's/^listen = .*/listen = 0.0.0.0:9000/' /etc/php/8.2/fpm/pool.d/www.conf
 
+
+chown -R www-data:www-data /var/www/html
 
 php-fpm8.2 -F
 
